feaffe49ca16e7072b6ce011c439319a4809c96d
wenshi <373872690@qq.com>
2019-05-24 18:26:57 +0800

    "init"

--- app/src/main/java/com/mei/financial/ui/sound/SoundRegisterActivity.java
+++ app/src/main/java/com/mei/financial/ui/sound/SoundRegisterActivity.java
@@ -1,7 +1,12 @@
 package com.mei.financial.ui.sound;
 
+import android.Manifest;
+
 import com.mei.financial.R;
 import com.meis.base.mei.base.BaseActivity;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.functions.Consumer;
 
 /**
  * @author wenshi
@@ -17,7 +22,17 @@ public class SoundRegisterActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        new RxPermissions(mContext).request(new String[]
+                {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.LOCATION_HARDWARE, Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.WRITE_SETTINGS, Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_CONTACTS})
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean granted) throws Exception {
 
+                    }
+                });
     }
 
     @Override
