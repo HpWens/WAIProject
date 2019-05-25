0b6652802512ed29435d4cbc69144153c743a140
ws@flytone.cn <ws@flytone.cn>
2019-05-25 22:00:53 +0800

    init

--- app/src/main/java/com/mei/financial/MyApplication.java
+++ app/src/main/java/com/mei/financial/MyApplication.java
@@ -3,6 +3,7 @@ package com.mei.financial;
 import android.app.Application;
 
 import com.iflytek.cloud.SpeechUtility;
+import com.vondear.rxtool.RxTool;
 
 /**
  * @author wenshi
@@ -15,6 +16,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         SpeechUtility.createUtility(MyApplication.this, "appid=" + getString(R.string.app_id));
+        RxTool.init(this);
         super.onCreate();
     }
 }
--- app/src/main/java/com/mei/financial/ui/sound/SoundRegisterActivity.java
+++ app/src/main/java/com/mei/financial/ui/sound/SoundRegisterActivity.java
@@ -1,11 +1,37 @@
 package com.mei.financial.ui.sound;
 
 import android.Manifest;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
+import com.iflytek.cloud.SpeechSynthesizer;
+import com.iflytek.cloud.SynthesizerListener;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
 import com.mei.financial.R;
+import com.mei.financial.utils.JsonParser;
 import com.meis.base.mei.base.BaseActivity;
 import com.tbruyelle.rxpermissions2.RxPermissions;
+import com.vondear.rxtool.view.RxToast;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.reactivex.functions.Consumer;
 
 /**
@@ -15,9 +41,38 @@ import io.reactivex.functions.Consumer;
  * @since 2019/5/23
  */
 public class SoundRegisterActivity extends BaseActivity {
+    @BindView(R.id.iv_header)
+    ImageView mIvHeader;
+    @BindView(R.id.tv_title)
+    TextView mTvTitle;
+    @BindView(R.id.tv_description)
+    TextView mTvDescription;
+    @BindView(R.id.tv_guide)
+    TextView mTvGuide;
+    @BindView(R.id.tv_sound_content)
+    TextView mTvSoundContent;
+    @BindView(R.id.iv_play)
+    ImageView mIvPlay;
+    @BindView(R.id.tv_dynamic_number)
+    TextView mTvDynamicNumber;
+    @BindView(R.id.tv_count)
+    TextView mTvCount;
+    @BindView(R.id.cv_sound)
+    CardView mCvSound;
+    @BindView(R.id.btn_verify)
+    TextView mBtnVerify;
+    @BindView(R.id.tv_about)
+    TextView mTvAbout;
+    @BindView(R.id.tv_record)
+    TextView mTvRecord;
+    @BindView(R.id.iv_record)
+    ImageView mIvRecord;
+    @BindView(R.id.layout_sound)
+    LinearLayout mLayoutSound;
+
     @Override
     protected void initView() {
-
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -26,17 +81,214 @@ public class SoundRegisterActivity extends BaseActivity {
                 {Manifest.permission.WRITE_EXTERNAL_STORAGE,
                         Manifest.permission.LOCATION_HARDWARE, Manifest.permission.READ_PHONE_STATE,
                         Manifest.permission.WRITE_SETTINGS, Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_CONTACTS})
+                        Manifest.permission.RECORD_AUDIO})
                 .subscribe(new Consumer<Boolean>() {
                     @Override
                     public void accept(Boolean granted) throws Exception {
-
+                        if (granted) {
+                            Log.e("SoundRegisterActivity", "initData--------dddd");
+                        }
                     }
                 });
+
+        // 初始化识别无UI识别对象
+        // 使用SpeechRecognizer对象，可根据回调消息自定义界面；
+        mIat = SpeechRecognizer.createRecognizer(this, new InitListener() {
+            @Override
+            public void onInit(int code) {
+                if (code != ErrorCode.SUCCESS) {
+                    // 错误码
+                }
+            }
+        });
+
+        mIatDialog = new RecognizerDialog(this, new InitListener() {
+            @Override
+            public void onInit(int code) {
+
+            }
+        });
+
+        mTts = SpeechSynthesizer.createSynthesizer(this, new InitListener() {
+            @Override
+            public void onInit(int code) {
+                if (code != ErrorCode.SUCCESS) {
+                    // 错误码
+                }
+            }
+        });
     }
 
     @Override
     protected int layoutResId() {
         return R.layout.sound_register_activity;
+    }
+
+    // 语音合成对象
+    private SpeechSynthesizer mTts;
+    // 默认发音人
+    private String voicer = "xiaoyan";
+
+
+    // 语音听写对象
+    private SpeechRecognizer mIat;
+    // 语音听写UI
+    private RecognizerDialog mIatDialog;
+    // 用HashMap存储听写结果
+    private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
+
+    private String mEngineType = SpeechConstant.TYPE_CLOUD;
+    private boolean mTranslateEnable = false;
+    private String resultType = "json";
+
+    public void setParam() {
+        // 清空参数
+        mIat.setParameter(SpeechConstant.PARAMS, null);
+        // 设置听写引擎
+        mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
+        // 设置返回结果格式
+        mIat.setParameter(SpeechConstant.RESULT_TYPE, resultType);
+        // 设置语言
+        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        // 设置语言区域
+        mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
+
+        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
+        mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
+
+        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
+        mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
+
+        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+        mIat.setParameter(SpeechConstant.ASR_PTT, "0");
+
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        mIat.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
+        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/iat.wav");
+
+    }
+
+    @OnClick({R.id.iv_play, R.id.btn_verify, R.id.iv_record})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_play:
+                if (null == mTts) {
+                    return;
+                }
+                setPlayParam();
+                int code = mTts.startSpeaking("2,3,4,5,6,7", new SynthesizerListener() {
+                    @Override
+                    public void onSpeakBegin() {
+
+                    }
+
+                    @Override
+                    public void onBufferProgress(int i, int i1, int i2, String s) {
+
+                    }
+
+                    @Override
+                    public void onSpeakPaused() {
+
+                    }
+
+                    @Override
+                    public void onSpeakResumed() {
+
+                    }
+
+                    @Override
+                    public void onSpeakProgress(int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onCompleted(SpeechError error) {
+                        if (error == null) {
+                        } else if (error != null) {
+                            RxToast.error(error.getPlainDescription(true));
+                        }
+                    }
+
+                    @Override
+                    public void onEvent(int i, int i1, int i2, Bundle bundle) {
+
+                    }
+                });
+
+                if (code != ErrorCode.SUCCESS) {
+                    RxToast.error("语音合成失败,错误码: " + code + ",请点击网址https://www.xfyun.cn/document/error-code查询解决方案");
+                }
+                break;
+            case R.id.btn_verify:
+                mLayoutSound.setVisibility(View.VISIBLE);
+                break;
+            case R.id.iv_record:
+                if (null == mIat) {
+                    // 创建单例失败，与 21001 错误为同样原因，参考 http://bbs.xfyun.cn/forum.php?mod=viewthread&tid=9688
+                    return;
+                }
+                // 设置参数
+                setParam();
+                mIatDialog.setListener(new RecognizerDialogListener() {
+                    @Override
+                    public void onResult(RecognizerResult results, boolean isLast) {
+                        printResult(results);
+                    }
+
+                    @Override
+                    public void onError(SpeechError error) {
+                        error.getPlainDescription(true);
+                    }
+                });
+                mIatDialog.show();
+                break;
+        }
+    }
+
+    private void setPlayParam() {
+        // 清空参数
+        mTts.setParameter(SpeechConstant.PARAMS, null);
+        mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
+
+        // 设置在线合成发音人
+        mTts.setParameter(SpeechConstant.VOICE_NAME, voicer);
+        //设置合成语速
+        mTts.setParameter(SpeechConstant.SPEED, "50");
+        //设置合成音调
+        mTts.setParameter(SpeechConstant.PITCH, "50");
+        //设置合成音量
+        mTts.setParameter(SpeechConstant.VOLUME, "50");
+
+        //设置播放器音频流类型
+        mTts.setParameter(SpeechConstant.STREAM_TYPE, "3");
+        // 设置播放合成音频打断音乐播放，默认为true
+        mTts.setParameter(SpeechConstant.KEY_REQUEST_FOCUS, "true");
+
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        mTts.setParameter(SpeechConstant.AUDIO_FORMAT, "pcm");
+        mTts.setParameter(SpeechConstant.TTS_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/tts.pcm");
+    }
+
+    private void printResult(RecognizerResult results) {
+        String text = JsonParser.parseIatResult(results.getResultString());
+        RxToast.normal(text);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (null != mTts) {
+            mTts.stopSpeaking();
+            // 退出时释放连接
+            mTts.destroy();
+        }
+
+        if (null != mIat) {
+            // 退出时释放连接
+            mIat.cancel();
+            mIat.destroy();
+        }
     }
 }
