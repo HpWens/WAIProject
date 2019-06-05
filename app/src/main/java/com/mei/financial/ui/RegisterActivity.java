62dafbe48c7d1b47638cb986ea62de006cc20c56
wenshi <373872690@qq.com>
2019-06-05 18:26:28 +0800

    init

--- app/src/main/java/com/mei/financial/ui/RegisterActivity.java
+++ app/src/main/java/com/mei/financial/ui/RegisterActivity.java
@@ -82,7 +82,7 @@ public class RegisterActivity extends BaseActivity implements CancelAdapt {
         mCbItem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
+                if (!isChecked) {
                     new UserProtocolDialog(mContext).show();
                 }
             }
